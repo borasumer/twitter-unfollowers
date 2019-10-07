d23806bafc66379f259f39bf72a654614512da38
Bora Sumer <solasguitarra@hotmail.com>
2019-10-06 22:00:01 -0400

    first commit

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 const express = require('express')
-const Twitter = require('twitter')
+const userList = require('user-list')
 
 const app = express()
-
-var client = new Twitter({
-  consumer_key: 'WBSjO0c2ednAJfdsIKOTuK5TU',
-  consumer_secret: 'cHJYRxp6RDobzha7sKtmfWS2Y4JYT4cCGB9JEoxsUL0jfBlcO2',
-  access_token_key: '828556655654797312-uwOWZJh1t3jW6ge7K1WunyLoeXoWmyX',
-  access_token_secret: 'elW8m0SKxRj8y9ZOgsfYmqKPtfxg74g9CfXqKUEThIbue'
-});
+app.use(express.json())
+app.use('view engine', 'pug')
 
 const port = process.env.port || 5000
 
@@ -16,26 +11,6 @@ app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`)
 })
 
-var params = { screen_name: 'allrational' }
-var not_followers = []
-app.get('/followers', (req, res) => {
-
-  client.get('followers/ids', params, function (error, data, response) {
-    var followers = data.ids
-
-    client.get('friends/ids', params, function (error, data, response) {
-      var friends = data.ids
-      friends.forEach((person) => {
-        if (followers.indexOf(person) === -1) {
-          not_followers.push(person)
-        }
-      })
-    })
-    not_followers = not_followers.slice(0, 99)
-    var not_followers_string = not_followers.join()
-    client.get('users/lookup', { user_id: not_followers_string }, function (error, data, response) {
-      console.log(data)
-    })
-  })
+app.get('/', (req, res) => {
 
 })
---
+++ user-list.js
@@ -0,0 +1,54 @@
+const Twitter = require('twitter')
+
+
+var client = new Twitter({
+  consumer_key: 'WBSjO0c2ednAJfdsIKOTuK5TU',
+  consumer_secret: 'cHJYRxp6RDobzha7sKtmfWS2Y4JYT4cCGB9JEoxsUL0jfBlcO2',
+  access_token_key: '828556655654797312-uwOWZJh1t3jW6ge7K1WunyLoeXoWmyX',
+  access_token_secret: 'elW8m0SKxRj8y9ZOgsfYmqKPtfxg74g9CfXqKUEThIbue'
+});
+
+
+var params = { screen_name: 'allrational' }
+var non_followers = []
+var users_nonfollowers = []
+
+client.get('followers/ids', params, (error, followers_result, response) => {
+  if (error) {
+    throw error
+  }
+  var followers = followers_result.ids
+
+  client.get('friends/ids', params, (error, friends_result, response) => {
+    if (error) {
+      throw error
+    }
+    var following = friends_result.ids
+
+    following.forEach(person => {
+      if (followers.indexOf(person) === -1) {
+        non_followers.push(person)
+      }
+    })
+    //console.log(non_followers)
+    //non_followers = non_followers.slice(0, 10)
+    var ids = non_followers.join()
+    client.get('users/lookup', { user_id: ids }, (error, users, response) => {
+      users.map(user => {
+        var userObject = {
+          name: user.name,
+          screen_name: user.screen_name,
+          avatar: user.profile_image_url
+        }
+        users_nonfollowers.push(userObject)
+      })
+      console.log(users_nonfollowers)
+    })
+  })
+})
+
+
+
+
+
+
