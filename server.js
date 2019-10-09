024eb9be4ef8988a3d4fba283fecdbd158d207b0
Bora Sumer <solasguitarra@hotmail.com>
2019-10-09 00:11:43 -0400

    added search unfollower users func

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express')
-const userList = require('user-list')
+const userList = require('./user-list')
+const bodyParser = require('body-parser')
 
 const app = express()
-app.use(express.json())
-app.use('view engine', 'pug')
-
-const port = process.env.port || 5000
-
-app.listen(port, () => {
-  console.log(`Server is listening on port: ${port}`)
-})
+app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(express.json())
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
+  res.render('index')
+})
 
+app.post('/get_users', (req, res) => {
+  var screen_name = req.body.handle
+  var users = userList(res, screen_name)
+})
+app.post('/unfollow_user', (req, res) => {
+  var screen_name = req.body.handle
+  var users = userList(res, screen_name)
+})
+
+const port = process.env.port || 3000
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`)
 })
--- user-list.js
+++ user-list.js
@@ -1,54 +1,50 @@
 const Twitter = require('twitter')
 
+module.exports = (res, screen_name) => {
+  var client = new Twitter({
+    consumer_key: 'WBSjO0c2ednAJfdsIKOTuK5TU',
+    consumer_secret: 'cHJYRxp6RDobzha7sKtmfWS2Y4JYT4cCGB9JEoxsUL0jfBlcO2',
+    access_token_key: '828556655654797312-uwOWZJh1t3jW6ge7K1WunyLoeXoWmyX',
+    access_token_secret: 'elW8m0SKxRj8y9ZOgsfYmqKPtfxg74g9CfXqKUEThIbue'
+  });
 
-var client = new Twitter({
-  consumer_key: 'WBSjO0c2ednAJfdsIKOTuK5TU',
-  consumer_secret: 'cHJYRxp6RDobzha7sKtmfWS2Y4JYT4cCGB9JEoxsUL0jfBlcO2',
-  access_token_key: '828556655654797312-uwOWZJh1t3jW6ge7K1WunyLoeXoWmyX',
-  access_token_secret: 'elW8m0SKxRj8y9ZOgsfYmqKPtfxg74g9CfXqKUEThIbue'
-});
 
+  var params = { screen_name }
+  var non_followers = []
+  var users_nonfollowers = []
 
-var params = { screen_name: 'allrational' }
-var non_followers = []
-var users_nonfollowers = []
-
-client.get('followers/ids', params, (error, followers_result, response) => {
-  if (error) {
-    throw error
-  }
-  var followers = followers_result.ids
-
-  client.get('friends/ids', params, (error, friends_result, response) => {
+  client.get('followers/ids', params, (error, followers_result, response) => {
     if (error) {
-      throw error
+      throw error;
     }
-    var following = friends_result.ids
+    var followers = followers_result.ids
 
-    following.forEach(person => {
-      if (followers.indexOf(person) === -1) {
-        non_followers.push(person)
+
+    client.get('friends/ids', params, (error, friends_result, response) => {
+      if (error) {
+        throw error
       }
-    })
-    //console.log(non_followers)
-    //non_followers = non_followers.slice(0, 10)
-    var ids = non_followers.join()
-    client.get('users/lookup', { user_id: ids }, (error, users, response) => {
-      users.map(user => {
-        var userObject = {
-          name: user.name,
-          screen_name: user.screen_name,
-          avatar: user.profile_image_url
+      var following = friends_result.ids
+
+      following.forEach(person => {
+        if (followers.indexOf(person) === -1) {
+          non_followers.push(person)
         }
-        users_nonfollowers.push(userObject)
       })
-      console.log(users_nonfollowers)
+      //console.log(non_followers)
+      //non_followers = non_followers.slice(0, 10)
+      var ids = non_followers.join()
+      client.get('users/lookup', { user_id: ids }, (error, users, response) => {
+        users.map(user => {
+          var userObject = {
+            name: user.name,
+            screen_name: user.screen_name,
+            avatar: user.profile_image_url
+          }
+          users_nonfollowers.push(userObject)
+        })
+        res.render('list', { users: users_nonfollowers })
+      })
     })
   })
-})
-
-
-
-
-
-
+}
